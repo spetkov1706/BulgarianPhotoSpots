c599cd0f8f6cad4b21eaf263ae6b4945f1a2fb63
spetkov1706 <s_petkov06@mail.bg>
2026-02-09 17:45:03 +0200

    Add PhotoSpotsController with basic routing

---
+++ BulgarianPhotoSpots/Controllers/PhotoSpotsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulgarianPhotoSpots.Controllers
+{
+    public class PhotoSpotsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("PhotoSpots Index page");
+        }
+        public IActionResult Details(int id)
+        {
+            return Content($"Photo spot details for id = {id}");
+        }
+    }
+}
