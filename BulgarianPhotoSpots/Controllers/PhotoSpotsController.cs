1740ab234c4ae878e1a05eaf26537128b56fd3fc
spetkov1706 <s_petkov06@mail.bg>
2026-02-09 20:20:52 +0200

    Add About action and view for PhotoSpots

--- BulgarianPhotoSpots/Controllers/PhotoSpotsController.cs
+++ BulgarianPhotoSpots/Controllers/PhotoSpotsController.cs
@@ -6,23 +6,22 @@ namespace BulgarianPhotoSpots.Controllers
     {
         public IActionResult Index()
         {
-            var photoSpots = new List<string>
-            {
-                "Седемте рилски езера",
-                "Белоградчишките скали",
-                "Нос Калиакра",
-                "Мелнишките пирамиди",
-                "Рилски манастир"
-            };
-
-            ViewData["Title"] = "Photo Spots in Bulgaria";
-            ViewData["Description"] = "Explore beautiful photo spots in Bulgaria";
-            ViewData["PhotoSpots"] = photoSpots;
             return View();
         }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             return View();
         }
+
     }
 }
