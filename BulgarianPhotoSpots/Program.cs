93b80fa4294f2dba05664ebcce95833d1c5b32ee
spetkov1706 <s_petkov06@mail.bg>
2026-02-12 21:14:36 +0200

    Register ApplicationDbContext in Program.cs

--- BulgarianPhotoSpots/Program.cs
+++ BulgarianPhotoSpots/Program.cs
@@ -1,3 +1,6 @@
+using BulgarianPhotoSpots.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BulgarianPhotoSpots
 {
     public class Program
@@ -8,6 +11,9 @@ namespace BulgarianPhotoSpots
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
