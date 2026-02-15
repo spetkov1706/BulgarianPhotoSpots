cd076125b8b6fff002751690fe8a4dd835390745
spetkov1706 <s_petkov06@mail.bg>
2026-02-15 20:53:03 +0200

    Created ICategoryService interface and implemented CategoryService with async data methods

---
+++ BulgarianPhotoSpots/Services/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using BulgarianPhotoSpots.Data;
+using BulgarianPhotoSpots.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulgarianPhotoSpots.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
