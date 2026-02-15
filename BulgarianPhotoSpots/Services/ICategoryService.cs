e02757732a7afdd59656ddb193701f4f986eb967
spetkov1706 <s_petkov06@mail.bg>
2026-02-15 20:47:03 +0200

    Created ICategoryService interface

---
+++ BulgarianPhotoSpots/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using BulgarianPhotoSpots.Models;
+
+namespace BulgarianPhotoSpots.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+    }
+}
