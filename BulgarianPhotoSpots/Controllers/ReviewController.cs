f71151b0003f1de000bc8b0a54ac023349fc0eb7
spetkov1706 <s_petkov06@mail.bg>
2026-03-30 15:05:50 +0300

    Add ReviewController with basic Index action

---
+++ BulgarianPhotoSpots/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulgarianPhotoSpots.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
