d2a7f97974f2730bb7bf398cd7e1d3cc06ac45e0
spetkov1706 <s_petkov06@mail.bg>
2026-03-31 11:20:53 +0300

    Add migration for Favorites entity

---
+++ BulgarianPhotoSpots.Infrastructure/Migrations/20260331081954_AddFavorites.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulgarianPhotoSpots.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favorite",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhotoSpotId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favorite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Favorite_PhotoSpots_PhotoSpotId",
+                        column: x => x.PhotoSpotId,
+                        principalTable: "PhotoSpots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Favorite_PhotoSpotId",
+                table: "Favorite",
+                column: "PhotoSpotId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favorite");
+        }
+    }
+}
