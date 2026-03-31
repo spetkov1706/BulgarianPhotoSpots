5639af053ccc5eb808ae968b4f0944265e965f33
spetkov1706 <s_petkov06@mail.bg>
2026-03-31 11:15:16 +0300

    Add Favorite entity model

---
+++ BulgarianPhotoSpots.Core/Models/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using BulgarianPhotoSpots.Models;
+
+namespace BulgarianPhotoSpots.Core.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public int PhotoSpotId { get; set; }
+
+        public PhotoSpot PhotoSpot { get; set; }
+    }
+}
