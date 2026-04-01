9347c06d6276a68bacf6401da2b03963bec47126
spetkov1706 <s_petkov06@mail.bg>
2026-04-01 10:57:08 +0300

    Create UserProfileController with MyProfile and Create actions

---
+++ BulgarianPhotoSpots/Controllers/UserProfileController.cs
@@ -0,0 +1,46 @@
+﻿using BulgarianPhotoSpots.Core.Models;
+using BulgarianPhotoSpots.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulgarianPhotoSpots.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult MyProfile()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var profile = _context.UserProfiles
+                .FirstOrDefault(p => p.UserId == userId);
+
+            return View(profile);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserProfile model)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            model.UserId = userId;
+            model.CreatedOn = DateTime.Now;
+
+            _context.UserProfiles.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("MyProfile");
+        }
+    }
+}
