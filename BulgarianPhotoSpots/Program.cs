25a636d52b10c6c5f532ad44dfdf97a49196831c
spetkov1706 <s_petkov06@mail.bg>
2026-04-07 13:02:48 +0300

    Implement Admin area with proper structure, routing and authorization

--- BulgarianPhotoSpots/Program.cs
+++ BulgarianPhotoSpots/Program.cs
@@ -90,6 +90,10 @@ namespace BulgarianPhotoSpots
 
             app.MapStaticAssets();
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
